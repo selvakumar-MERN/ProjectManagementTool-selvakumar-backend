e76d1d1dab43719f0c02b96dcce61ed8fdd33b6b
selvakumar k <84513538+selvakumar-MERN@users.noreply.github.com>
2023-07-31 17:48:55 +0530

    Update project.js

--- contoller/project.js
+++ contoller/project.js
@@ -1,4 +1,6 @@
 const user=require('../model/users')
+
+//Creating a project by admin
 const createProject=async(req,res)=>{
     const{email,projectName,projectOwner,startDate,finishDate,status}=req.body
 try{    
@@ -10,6 +12,7 @@ catch(error){
 }
 }
 
+//getting the project created by admin
 const getProject=async(req,res)=>{
     const {email}=req.body
     
@@ -24,5 +27,37 @@ catch(error){
 }
 
 }
+
+//editing the project by admin
+const updateproject=async(req,res)=>{
+    const{id}=req.params
+    const{projectName,projetOwner,startDate,finishDate,status}=req.body
+try{    
+    const find= await user.findOne({Project:{$elemMatch:{_id:id}}})
+    
+     await user.updateOne({email:find.email,"Project._id":id},{$set:{"Project.$.projectName":projectName,"Project.$.projetOwner":projetOwner,"Project.$.startDate":startDate,"Project.$.finishDate":finishDate,"Project.$.status":status}})
+       res.status(200).send("Update sucessfully")
+}
+catch(error)
+{
+    res.status(400).send(error)
+}
+}
+
+//Deleting the project by admin
+const deleteproject=async(req,res)=>{
+    const{id}=req.params
+    const{email}=req.body
+    console.log(id,email)
+ try{
+       await user.updateOne({email:email},{$pull:{"Project":{"_id":id}}})
+       res.status(200).send(" Project Deleted sucessfully")
+ } 
+ catch(error){
+       res.status(400).send(error)
+ }  
+}
+module.exports.deleteproject=deleteproject;
+module.exports.updateproject=updateproject;
 module.exports.getProject=getProject;
 module.exports.createProject=createProject;
